6a154c46ae52418f05d975d14bb67e55bdf6cefe
kaiyu.shan <kaiyu.shan@outlook.com>
2024-05-13 14:06:51 +0800

    feat(astro): 开启node ssr

--- src/config/astro.config.mjs
+++ src/config/astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,6 +13,10 @@ export default defineConfig({
       theme: 'dark-plus'
     }
   },
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  }),
   integrations: [mdx()],
   srcDir: './src/html',
   cacheDir: './dist/pages',
