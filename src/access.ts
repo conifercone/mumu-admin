d029e1bb20afa5aaa6bc00250de5075764061857
kaiyu.shan <kaiyu.shan@outlook.com>
2025-02-06 16:08:53 +0800

    refactor(mumu): 采用vuetify实现

--- src/access.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @see https://umijs.org/docs/max/access#access
- * */
-export default function access(initialState: { currentUser?: API.CurrentUser } | undefined) {
-  const { currentUser } = initialState ?? {};
-  return {
-    canAdmin: currentUser && currentUser.roles.some((obj) => obj.code === 'admin'),
-  };
-}
