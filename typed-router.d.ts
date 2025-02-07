6beed281e1cf9ed678e61bdfe7f5964eae573296
kaiyu.shan <kaiyu.shan@outlook.com>
2025-02-07 17:36:57 +0800

    feat(pages): 新增登录页和注册页

--- typed-router.d.ts
+++ typed-router.d.ts
@@ -19,5 +19,7 @@ declare module 'vue-router/auto-routes' {
    */
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
+    '/Login': RouteRecordInfo<'/Login', '/Login', Record<never, never>, Record<never, never>>,
+    '/Register': RouteRecordInfo<'/Register', '/Register', Record<never, never>, Record<never, never>>,
   }
 }
