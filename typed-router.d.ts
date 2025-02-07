1654e007a97a4adf097436bb57667883dc43b2f5
kaiyu.shan <kaiyu.shan@outlook.com>
2025-02-07 17:53:56 +0800

    feat(pages): 新增关于界面

--- typed-router.d.ts
+++ typed-router.d.ts
@@ -19,6 +19,7 @@ declare module 'vue-router/auto-routes' {
    */
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
+    '/About': RouteRecordInfo<'/About', '/About', Record<never, never>, Record<never, never>>,
     '/Login': RouteRecordInfo<'/Login', '/Login', Record<never, never>, Record<never, never>>,
     '/Register': RouteRecordInfo<'/Register', '/Register', Record<never, never>, Record<never, never>>,
     '/user/Profile': RouteRecordInfo<'/user/Profile', '/user/Profile', Record<never, never>, Record<never, never>>,
