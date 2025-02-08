b079d726c23f258cb76d25fcc9ff342a55b91021
kaiyu.shan <kaiyu.shan@outlook.com>
2025-02-08 17:13:16 +0800

    refactor(login): 修改登录页

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,49 +1,94 @@
-<script setup lang="ts">
-import { useTheme } from 'vuetify'
+<script setup>
+import { ref } from 'vue'
 
-const { global } = useTheme()
+const visible = ref(false)
+</script>
 
-const authProviders = [
-  {
-    icon: 'mdi-facebook',
-    color: '#4267b2',
-    colorInDark: '#4267b2',
-  },
-  {
-    icon: 'mdi-twitter',
-    color: '#1da1f2',
-    colorInDark: '#1da1f2',
-  },
-  {
-    icon: 'mdi-github',
-    color: '#272727',
-    colorInDark: '#fff',
-  },
-  {
-    icon: 'mdi-google',
-    color: '#db4437',
-    colorInDark: '#db4437',
-  },
-]
+<script>
+export default {
+  data: () => ({
+    visible: false,
+  }),
+}
 </script>
 
 <template>
-  <v-container class="fill-height">
-    <v-responsive
-      class="align-centerfill-height mx-auto"
-      max-width="900"
+  <div>
+    <v-img
+      class="mb-4"
+      height="150"
+      src="@/assets/logo.png"
+    />
+
+    <v-card
+      class="mx-auto pa-12 pb-8"
+      elevation="8"
+      max-width="448"
+      rounded="lg"
     >
-      <v-btn
-        v-for="link in authProviders"
-        :key="link.icon"
-        :icon="link.icon"
-        variant="text"
-        :color="global.name.value === 'dark' ? link.colorInDark : link.color"
+      <div class="text-subtitle-1 text-medium-emphasis">
+        Account
+      </div>
+
+      <v-text-field
+        density="compact"
+        placeholder="Email address"
+        prepend-inner-icon="mdi-email-outline"
+        variant="outlined"
       />
-    </v-responsive>
-  </v-container>
+
+      <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+        Password
+
+        <a
+          class="text-caption text-decoration-none text-blue"
+          href="#"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Forgot login password?</a>
+      </div>
+
+      <v-text-field
+        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+        :type="visible ? 'text' : 'password'"
+        density="compact"
+        placeholder="Enter your password"
+        prepend-inner-icon="mdi-lock-outline"
+        variant="outlined"
+        @click:append-inner="visible = !visible"
+      />
+
+      <v-card
+        class="mb-12"
+        color="surface-variant"
+        variant="tonal"
+      >
+        <v-card-text class="text-medium-emphasis text-caption">
+          Warning: After 3 consecutive failed login attempts, you account will be temporarily locked for three hours. If you must login now, you can also click "Forgot login password?" below to reset the login password.
+        </v-card-text>
+      </v-card>
+
+      <v-btn
+        class="mb-8"
+        color="blue"
+        size="large"
+        variant="tonal"
+        block
+      >
+        Log In
+      </v-btn>
+
+      <v-card-text class="text-center">
+        <a
+          class="text-blue text-decoration-none"
+          href="/register"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Sign up now <v-icon icon="mdi-chevron-right" />
+        </a>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
