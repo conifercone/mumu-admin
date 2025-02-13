5b94ff0ed6abe152ab602fec377f715c7632e22e
kaiyu.shan <kaiyu.shan@outlook.com>
2025-02-13 17:49:20 +0800

    refactor(login): 登录页增加背景图

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -36,97 +36,105 @@ async function onSubmit() {
 </script>
 
 <template>
-  <div>
-    <v-img
-      class="mb-4"
-      height="150"
-      src="@/assets/logo.png"
-    />
-
-    <v-card
-      class="mx-auto pa-12 pb-8"
-      elevation="8"
-      max-width="448"
-      rounded="lg"
+  <v-img
+    src="@/assets/login-background.jpg"
+    height="100vh"
+    cover
+  >
+    <v-container
+      class="d-flex justify-center align-center fill-height"
+      fluid
     >
-      <v-form
-        v-model="form"
-        fast-fail
-        @submit.prevent="onSubmit"
+      <v-card
+        class="mx-auto pa-12 pb-8"
+        elevation="8"
+        max-width="448"
+        rounded="lg"
       >
-        <div class="text-subtitle-1 text-medium-emphasis">
-          Account
-        </div>
-
-        <v-text-field
-          v-model="username"
-          density="compact"
-          placeholder="Enter your username"
-          prepend-inner-icon="mdi-account-circle-outline"
-          :rules="[rules.required]"
-          variant="outlined"
-          clearable
+        <v-img
+          class="mb-4"
+          height="150"
+          src="@/assets/logo.png"
         />
-
-        <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-          Password
-
-          <a
-            class="text-caption text-decoration-none text-blue"
-            href="#"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Forgot login password?</a>
-        </div>
-
-        <v-text-field
-          v-model="password"
-          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-          :type="visible ? 'text' : 'password'"
-          density="compact"
-          placeholder="Enter your password"
-          prepend-inner-icon="mdi-lock-outline"
-          :rules="[rules.required]"
-          variant="outlined"
-          clearable
-          @click:append-inner="visible = !visible"
-        />
-
-        <v-card
-          class="mb-12"
-          color="surface-variant"
-          variant="tonal"
+        <v-form
+          v-model="form"
+          fast-fail
+          @submit.prevent="onSubmit"
         >
-          <v-card-text class="text-medium-emphasis text-caption">
-            Warning: After 3 consecutive failed login attempts, you account will be temporarily locked for three hours. If you must login now, you can also click "Forgot login password?" below to reset the login password.
+          <div class="text-subtitle-1 text-medium-emphasis">
+            Account
+          </div>
+
+          <v-text-field
+            v-model="username"
+            density="compact"
+            placeholder="Enter your username"
+            prepend-inner-icon="mdi-account-circle-outline"
+            :rules="[rules.required]"
+            variant="outlined"
+            clearable
+          />
+
+          <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+            Password
+
+            <a
+              class="text-caption text-decoration-none text-blue"
+              href="#"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Forgot login password?</a>
+          </div>
+
+          <v-text-field
+            v-model="password"
+            :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+            :type="visible ? 'text' : 'password'"
+            density="compact"
+            placeholder="Enter your password"
+            prepend-inner-icon="mdi-lock-outline"
+            :rules="[rules.required]"
+            variant="outlined"
+            clearable
+            @click:append-inner="visible = !visible"
+          />
+
+          <v-card
+            class="mb-12"
+            color="surface-variant"
+            variant="tonal"
+          >
+            <v-card-text class="text-medium-emphasis text-caption">
+              Warning: After 3 consecutive failed login attempts, you account will be temporarily locked for three hours. If you must login now, you can also click "Forgot login password?" below to reset the login password.
+            </v-card-text>
+          </v-card>
+
+          <v-btn
+            :loading="loading"
+            class="mb-8"
+            color="blue"
+            size="large"
+            variant="tonal"
+            type="submit"
+            :disabled="!form"
+            block
+          >
+            Log In
+          </v-btn>
+
+          <v-card-text class="text-center">
+            <a
+              class="text-blue text-decoration-none"
+              href="/register"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Sign up now <v-icon icon="mdi-chevron-right" />
+            </a>
           </v-card-text>
-        </v-card>
-
-        <v-btn
-          :loading="loading"
-          class="mb-8"
-          color="blue"
-          size="large"
-          variant="tonal"
-          type="submit"
-          :disabled="!form"
-          block
-        >
-          Log In
-        </v-btn>
-
-        <v-card-text class="text-center">
-          <a
-            class="text-blue text-decoration-none"
-            href="/register"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Sign up now <v-icon icon="mdi-chevron-right" />
-          </a>
-        </v-card-text>
-      </v-form>
-    </v-card>
-  </div>
+        </v-form>
+      </v-card>
+    </v-container>
+  </v-img>
 </template>
