eb9b47d6bb83df2b5c285f492c11b194fa9b9c14
Kaiyu Shan <kaiyu.shan@outlook.com>
2026-01-07 10:10:06 +0800

    feat(mumu): 集成Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,16 @@
-import vuetify from 'eslint-config-vuetify'
+import prettier from "eslint-config-prettier";
+import vuetify from "eslint-config-vuetify";
+import prettierPlugin from "eslint-plugin-prettier";
 
-export default vuetify()
+export default [
+  ...vuetify(),
+  // 关闭所有与 Prettier 冲突的 ESLint 格式类规则
+  prettier,
+  // 让 ESLint 把 Prettier 结果当作规则报错（可选，但你原来想要这个）
+  {
+    plugins: { prettier: prettierPlugin },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+];
