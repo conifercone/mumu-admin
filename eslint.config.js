704a38e35cf3d2b90d6cfca89c37e34d0c674d8c
Kaiyu Shan <kaiyu.shan@outlook.com>
2026-01-08 20:33:27 +0800

    style(mumu): 格式化代码

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
-import prettier from "eslint-config-prettier";
-import vuetify from "eslint-config-vuetify";
-import prettierPlugin from "eslint-plugin-prettier";
+import prettier from 'eslint-config-prettier'
+import vuetify from 'eslint-config-vuetify'
+import prettierPlugin from 'eslint-plugin-prettier'
 
 export default [
   ...vuetify(),
   // 关闭所有与 Prettier 冲突的 ESLint 格式类规则
   prettier,
-];
+]
