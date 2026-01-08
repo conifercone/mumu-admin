d97e814429d34eef575b2ebf5bb90859230eb94b
Kaiyu Shan <kaiyu.shan@outlook.com>
2026-01-08 20:28:04 +0800

    feat(config): 添加配置文件

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -59,28 +59,28 @@
 <script setup lang="ts">
 const links = [
   {
-    href: "https://vuetifyjs.com/",
-    icon: "mdi-text-box-outline",
-    subtitle: "Learn about all things Vuetify in our documentation.",
-    title: "Documentation",
+    href: 'https://vuetifyjs.com/',
+    icon: 'mdi-text-box-outline',
+    subtitle: 'Learn about all things Vuetify in our documentation.',
+    title: 'Documentation',
   },
   {
-    href: "https://vuetifyjs.com/introduction/why-vuetify/#feature-guides",
-    icon: "mdi-star-circle-outline",
-    subtitle: "Explore available framework Features.",
-    title: "Features",
+    href: 'https://vuetifyjs.com/introduction/why-vuetify/#feature-guides',
+    icon: 'mdi-star-circle-outline',
+    subtitle: 'Explore available framework Features.',
+    title: 'Features',
   },
   {
-    href: "https://vuetifyjs.com/components/all",
-    icon: "mdi-widgets-outline",
-    subtitle: "Discover components in the API Explorer.",
-    title: "Components",
+    href: 'https://vuetifyjs.com/components/all',
+    icon: 'mdi-widgets-outline',
+    subtitle: 'Discover components in the API Explorer.',
+    title: 'Components',
   },
   {
-    href: "https://discord.vuetifyjs.com",
-    icon: "mdi-account-group-outline",
-    subtitle: "Connect with Vuetify developers.",
-    title: "Community",
+    href: 'https://discord.vuetifyjs.com',
+    icon: 'mdi-account-group-outline',
+    subtitle: 'Connect with Vuetify developers.',
+    title: 'Community',
   },
-];
+]
 </script>
