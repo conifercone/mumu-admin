d6b56d2fc58eb2a6c03c0b94c7ef56b72e5e658a
Kaiyu Shan <kaiyu.shan@outlook.com>
2026-01-20 11:21:38 +0800

    feat(router): 增加token判断

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,31 +5,44 @@
  */
 
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
-import { routes } from 'vue-router/auto-routes'
+import { createRouter, createWebHistory } from 'vue-router';
+import { routes } from 'vue-router/auto-routes';
+import { PUBLIC_ROUTES } from '@/utils/constants';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-})
+});
+
+router.beforeEach((to, _from, next) => {
+  const token = localStorage.getItem('token');
+
+  if (PUBLIC_ROUTES.includes(to.path)) {
+    next();
+  } else if (token) {
+    next();
+  } else {
+    next('/auth/login');
+  }
+});
 
 // Workaround for https://github.com/vitejs/vite/issues/11804
 router.onError((err, to) => {
   if (err?.message?.includes?.('Failed to fetch dynamically imported module')) {
     if (localStorage.getItem('vuetify:dynamic-reload')) {
-      console.error('Dynamic import error, reloading page did not fix it', err)
+      console.error('Dynamic import error, reloading page did not fix it', err);
     } else {
-      console.log('Reloading page to fix dynamic import error')
-      localStorage.setItem('vuetify:dynamic-reload', 'true')
-      location.assign(to.fullPath)
+      console.log('Reloading page to fix dynamic import error');
+      localStorage.setItem('vuetify:dynamic-reload', 'true');
+      location.assign(to.fullPath);
     }
   } else {
-    console.error(err)
+    console.error(err);
   }
-})
+});
 
 router.isReady().then(() => {
-  localStorage.removeItem('vuetify:dynamic-reload')
-})
+  localStorage.removeItem('vuetify:dynamic-reload');
+});
 
-export default router
+export default router;
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,3 +6,5 @@ export const OAUTH2_CONFIG = {
   clientId: 'mumu-client',
   clientSecret: 'mumu',
 };
+
+export const PUBLIC_ROUTES = ['/auth/login', '/auth/register'];
